b80aeb1404edab86693635453a971bb584a22b57
Alessandro Alfonso <alessandro_alfonso@dlsu.edu.ph>
2023-02-20 19:27:25 +0800

    Expense Tracker First Commit

--- App.js
+++ App.js
@@ -1,11 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
-export default function App() {
+export default function ExpenseTracker() {
+  const [expense, setExpense] = useState('');
+  const [expensesList, setExpensesList] = useState([]);
+
+  const handleAddExpense = () => {
+    if (expense) {
+      setExpensesList([...expensesList, expense]);
+      setExpense('');
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Expense Tracker</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter expense"
+          value={expense}
+          onChangeText={(text) => setExpense(text)}
+        />
+        <TouchableOpacity style={styles.button} onPress={handleAddExpense}>
+          <Text style={styles.buttonText}>Add</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.expensesList}>
+        {expensesList.map((item, index) => (
+          <Text key={index} style={styles.expenseItem}>
+            {item}
+          </Text>
+        ))}
+      </View>
     </View>
   );
 }
@@ -16,5 +49,43 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    marginTop:30,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+    width: '70%',
+    marginRight: 10,
+    borderRadius: 5,
+  },
+  button: {
+    backgroundColor: '#007AFF',
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  expensesList: {
+    flex: 1,
+    width: '100%',
+  },
+  expenseItem: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
   },
 });
